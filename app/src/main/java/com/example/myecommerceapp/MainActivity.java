993a80451bbf389eabb2f370e643db9ad8d7a671
Acer <bikashkatwalcr7@gmail.com>
2020-08-02 20:01:30 +0545

    Login and register design completed

--- app/src/main/java/com/example/myecommerceapp/MainActivity.java
+++ app/src/main/java/com/example/myecommerceapp/MainActivity.java
@@ -27,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/myecommerceapp/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.myecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccountButton = findViewById(R.id.register_btn);
+        InputName = findViewById(R.id.register_username_input);
+        InputPassword = findViewById(R.id.register_password_input);
+        InputPhoneNumber = findViewById(R.id.register_phone_number_input);
+
+        //5th video completed
+    }
+}
